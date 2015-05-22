f403c1a7337babe11f310f392f89dbeddeae174a
Matt Thompson <matthewaaronthompson@gmail.com>
2015-05-22 01:33:20 -0500

    ploog

--- plugdj/ploog.js
+++ plugdj/ploog.js
@@ -3,66 +3,59 @@ if (typeof(ploog) == "undefined") {
 		started: false,
 		reloadURL: "https://854.github.io/plugdj/ploog.js",
 		media:null,
+		queueInfo: null,
 		dj:null
 	};
 }
 
 ploog.init = function(){
-	console.log("-------------------------\nwtf is ploog?\nlaunching ploog.\n-------------------------");
-	ploog.api.init();
-	ploog.ui.build();
+	$.getScript("https://cdn.firebase.com/js/client/1.1.0/firebase.js", ploog.yeho);
 	ploog.started = true;
 };
 
-ploog.api = {
-	init: function(){
-		ploog.api.fill();
-		API.on(API.CHAT, ploog.api.newchat);
-		API.on(API.CHAT_COMMAND, ploog.api.newcmd);
-		API.on(API.DJ_ADVANCE, ploog.api.newsong);
-		API.on(API.USER_JOIN, ploog.api.newuser);
-		API.on(API.USER_LEAVE, ploog.api.newexit);
-		API.on(API.VOTE_UPDATE, ploog.api.newvote);
-		API.on(API.CURATE_UPDATE, ploog.api.newheart);
-		API.on(API.WAIT_LIST_UPDATE, ploog.api.waitlistupdate);
-	},
-	fill: function(){
-		ploog.media = API.getMedia();
-		ploog.dj = API.getDJ();
-	},
-	newcmd: function(cmd){
-		if (cmd == "/reload"){
-			console.log("killing ploog.");
-			ploog.reload();
+ploog.yeho = function(){
+	console.log("-------------------------\nlaunching Indie Discotheque Queue\n-------------------------");
+	ploog.queueInfo = new Firebase("https://discotheque-list.firebaseio.com/queue");
+	ploog.queueInfo.on("value", function(snapshot) {ploog.queueUpdate(snapshot);});
+	ploog.ui.build();
+};
+
+ploog.queueUpdate = function(snapshot){
+	var data = snapshot.val();
+	var queue = data.queue;
+	var stage = data.stage;
+	var queueString = "<br/><i>Queue</i><br/>";
+	var stageString = "<i>DJs</i><br/>";
+	if (queue == ""){
+		queueString += "The list is empty!<br/>";
+	} else {
+		var tListAry = queue.split("\\");
+		if (tListAry.length){
+			for (var i = 0; i < tListAry.length; i++){
+				if (tListAry[i]) queueString += tListAry[i]+"<br/>";
+			}
+		} else {
+			queueString += "The List is empty!<br/>";
 		}
-	},
-	newchat: function(data){
-
-	},
-	newsong: function(data){
-
-	},
-	newuser: function(data){
-
-	},
-	newexit: function(data){
-
-	},
-	newvote: function(data){
-
-	},
-	newheart: function(data){
-
-	},
-	waitlistupdate: function(data){
-
 	}
+	var djListAry = stage.split("\\");
+
+	if (djListAry.length){
+		for (var i = 0; i < djListAry.length; i++){
+				if (djListAry[i]) stageString += djListAry[i]+"<br/>";
+			}
+	} else {
+		stageString += "Nobody is DJing!";
+	}
+
+	$( "#ploog_screen" ).html(stageString+""+queueString);
+
 };
 
 ploog.ui = {
 	build: function(){
-		$( "body" ).prepend("<style id='ploog_styles'>#ploog_screen{padding-top:10px;padding-left:4px;height:108px; overflow-y:scroll;} #ploog_ui{z-index:9; font-family:helvetica,arial,sans-serif; left:12px; font-size:12px;height:140px; position:absolute; color:#000; top:70px; width:170px; background-color:#282C35; color:#d1d1d1;} #ploog_headr{line-height:22px;background-color:#1C1F25;font-weight:700;color:#d1d1d1;padding-left:4px;}</style><div id='ploog_ui'></div>");
-		$( "#ploog_ui" ).append("<div id='ploog_headr'>ploog</div>");
+		$( "body" ).prepend("<style id='ploog_styles'>#ploog_screen{padding-top:10px;padding-left:4px;height:210px; overflow-y:scroll;} #ploog_ui{z-index:9; font-family:helvetica,arial,sans-serif; left:12px; font-size:12px;height:250px; position:absolute; color:#000; top:70px; width:170px; background-color:#282C35; color:#d1d1d1;} #ploog_headr{line-height:22px;background-color:#1C1F25;font-weight:700;color:#d1d1d1;padding-left:4px;}</style><div id='ploog_ui'></div>");
+		$( "#ploog_ui" ).append("<div id='ploog_headr'>#NARF</div>");
 		$( "#ploog_ui" ).append("<div id='ploog_screen'>welcome</div>");
 	},
 	destroy: function(){
@@ -83,13 +76,6 @@ ploog.reload = function(){
 };
 
 ploog.cleanUp = function(){
-	API.off(API.CHAT, ploog.api.newchat);
-	API.off(API.DJ_ADVANCE, ploog.api.newsong);
-	API.off(API.USER_JOIN, ploog.api.newuser);
-	API.off(API.USER_LEAVE, ploog.api.newexit);
-	API.off(API.VOTE_UPDATE, ploog.api.newvote);
-	API.off(API.CURATE_UPDATE, ploog.api.newheart);
-	API.off(API.WAIT_LIST_UPDATE, ploog.api.waitlistupdate);
 	ploog.ui.destroy();
 	$("#ploog").remove();
 	ploog.started = false;
