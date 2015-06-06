c55d9363a866c1e87fa4331282ebdefb63016899
Matt Thompson <matthewaaronthompson@gmail.com>
2015-06-05 23:15:20 -0500

    cleam up ploog

--- plugdj/ploog.js
+++ plugdj/ploog.js
@@ -1,149 +1,159 @@
 if (typeof(ploog) == "undefined") {
-	var ploog = {
-		started: false,
-		fbLoaded: false
-	};
+    var ploog = {
+        started: false,
+        fbLoaded: false
+    };
 }
 
 ploog.username = "YM";
 ploog.onValueChange = null;
-ploog.init = function(){
-	if (!ploog.fbLoaded) {
-		$.getScript("https://cdn.firebase.com/js/client/1.1.0/firebase.js", ploog.yeho);
-	} else {
-		ploog.yeho();
-	}
-	ploog.started = true;
+ploog.init = function() {
+    if (!ploog.fbLoaded) {
+        $.getScript("https://cdn.firebase.com/js/client/1.1.0/firebase.js", ploog.yeho);
+    } else {
+        ploog.yeho();
+    }
+    ploog.started = true;
 };
 
-ploog.yeho = function(){
-	console.log("-------------------------\nlaunching Indie Discotheque Queue\n-------------------------");
-	ploog.usrname = API.getUser().username;
-	ploog.fbLoaded = true;
-	ploog.queueInfo = new Firebase("https://discotheque-list.firebaseio.com/queue");
-	ploog.onValueChange = ploog.queueInfo.on("value", function(snapshot) {ploog.queueUpdate(snapshot);});
-	ploog.ui.build();
+ploog.yeho = function() {
+    console.log("-------------------------\nlaunching Indie Discotheque Queue\n-------------------------");
+    ploog.usrname = API.getUser().username;
+    ploog.fbLoaded = true;
+    ploog.queueInfo = new Firebase("https://discotheque-list.firebaseio.com/queue");
+    ploog.onValueChange = ploog.queueInfo.on("value", function(snapshot) {
+        ploog.queueUpdate(snapshot);
+    });
+    ploog.ui.build();
 };
 
-ploog.queueUpdate = function(snapshot){
-	var data = snapshot.val();
-	var queue = data.queue;
-	var stage = data.stage;
-	var queueString = "<br/><strong>Queue</strong><br/>";
-	var stageString = "<strong>DJs</strong><br/>";
-	if (queue == ""){
-		queueString += "The list is empty!<br/>";
-	} else {
-		var tListAry = queue.split("\\");
-		if (tListAry.length){
-			for (var i = 0; i < tListAry.length; i++){
-				if (tListAry[i]){
-					var usrname = tListAry[i].substr(tListAry[i].indexOf(" ") + 1);
-					if (usrname == ploog.usrname){
-						queueString += "<span class=\"ploog_currentdj\">"+tListAry[i]+"</span><br/>";
-					} else {
-						queueString += tListAry[i]+"<br/>";
-					}
-				}
-			}
-		} else {
-			queueString += "The List is empty!<br/>";
-		}
-	}
-	var djListAry = stage.split("\\");
+ploog.queueUpdate = function(snapshot) {
+    var data = snapshot.val();
+    var queue = data.queue;
+    var stage = data.stage;
+    var queueString = "<br/><strong>Queue</strong><br/>";
+    var stageString = "<strong>DJs</strong><br/>";
+    if (queue == "") {
+        queueString += "The list is empty!<br/>";
+    } else {
+        var tListAry = queue.split("\\");
+        if (tListAry.length) {
+            for (var i = 0; i < tListAry.length; i++) {
+                if (tListAry[i]) {
+                    var usrname = tListAry[i].substr(tListAry[i].indexOf(" ") + 1);
+                    if (usrname == ploog.usrname) {
+                        queueString += "<span class=\"ploog_currentdj\">" + tListAry[i] + "</span><br/>";
+                    } else {
+                        queueString += tListAry[i] + "<br/>";
+                    }
+                }
+            }
+        } else {
+            queueString += "The List is empty!<br/>";
+        }
+    }
+    var djListAry = stage.split("\\");
 
-	if (djListAry.length){
-		for (var i = 0; i < 4; i++){
-				if (djListAry[i]){
-					var lastIndex = djListAry[i].lastIndexOf(" ");
-					var usrname =  djListAry[i].substring(0, lastIndex);
+    if (djListAry.length) {
+        for (var i = 0; i < 4; i++) {
+            if (djListAry[i]) {
+                var lastIndex = djListAry[i].lastIndexOf(" ");
+                var usrname = djListAry[i].substring(0, lastIndex);
 
-					if (usrname == ploog.usrname){
-						if (i == 0){
-							stageString += "<i><span class=\"ploog_currentdj\">"+djListAry[i]+"</span></i><br/>";
-						} else {
-							stageString += "<span class=\"ploog_currentdj\">"+djListAry[i]+"</span><br/>";
-						}
-					} else if (i == 0) {
-						stageString += "<i>"+djListAry[i]+"</i><br/>";
-					} else {
-						stageString += djListAry[i]+"<br/>";
-					}
-				} else {
-					stageString += "<span class=\"ploog_emptyspot\">unoccupied</span><br/>";
-				}
-			}
-	} else {
-		stageString += "Nobody is DJing!";
-	}
+                if (usrname == ploog.usrname) {
+                    if (i == 0) {
+                        stageString += "<i><span class=\"ploog_currentdj\">" + djListAry[i] + "</span></i><br/>";
+                    } else {
+                        stageString += "<span class=\"ploog_currentdj\">" + djListAry[i] + "</span><br/>";
+                    }
+                } else if (i == 0) {
+                    stageString += "<i>" + djListAry[i] + "</i><br/>";
+                } else {
+                    stageString += djListAry[i] + "<br/>";
+                }
+            } else {
+                stageString += "<span class=\"ploog_emptyspot\">unoccupied</span><br/>";
+            }
+        }
+    } else {
+        stageString += "Nobody is DJing!";
+    }
 
-	$( "#ploog_screen" ).html(stageString+""+queueString);
+    $("#ploog_screen").html(stageString + "" + queueString);
 
 };
 
 ploog.ui = {
-	build: function(){
-		$( "body" ).prepend("<style id='ploog_styles'>.ploog_emptyspot{color:#666;}.ploog_currentdj{color:#ffdd6f;}#ploog_screen{padding-top:10px;padding-left:4px;height:210px; overflow-y:scroll;} #ploog_ui{z-index:40; font-family:helvetica,arial,sans-serif; left:12px; font-size:12px;height:250px; position:absolute; color:#000; top:70px; width:170px; background-color:#282C35; color:#d1d1d1;} #ploog_headr{line-height:22px;background-color:#1C1F25;font-weight:700;color:#d1d1d1;padding-left:4px;}</style><div id='ploog_ui'></div>");
-		$( "#ploog_ui" ).append("<div id='ploog_headr'>#NARF</div>");
-		$( "#ploog_ui" ).append("<div id='ploog_screen'>Loading...</div>");
-		$("#ploog_ui").drags({handle:"#ploog_headr"});
-	},
-	destroy: function(){
-		$("#ploog_styles").remove();
-		$("#ploog_ui").remove();
-	}
+    build: function() {
+        $("body").prepend("<style id='ploog_styles'>.ploog_emptyspot{color:#666;}.ploog_currentdj{color:#ffdd6f;}#ploog_screen{padding-top:10px;padding-left:4px;height:210px; overflow-y:scroll;} #ploog_ui{z-index:40; font-family:helvetica,arial,sans-serif; left:12px; font-size:12px;height:250px; position:absolute; color:#000; top:70px; width:170px; background-color:#282C35; color:#d1d1d1;} #ploog_headr{line-height:22px;background-color:#1C1F25;font-weight:700;color:#d1d1d1;padding-left:4px;}</style><div id='ploog_ui'></div>");
+        $("#ploog_ui").append("<div id='ploog_headr'>#NARF</div>");
+        $("#ploog_ui").append("<div id='ploog_screen'>Loading...</div>");
+        $("#ploog_ui").drags({
+            handle: "#ploog_headr"
+        });
+    },
+    destroy: function() {
+        $("#ploog_styles").remove();
+        $("#ploog_ui").remove();
+    }
 }
 
-ploog.cleanUp = function(){
-	ploog.queueInfo.off("value", ploog.onValueChange);
-	ploog.ui.destroy();
-	$("#ploog").remove();
-	ploog.started = false;
+ploog.cleanUp = function() {
+    ploog.queueInfo.off("value", ploog.onValueChange);
+    ploog.ui.destroy();
+    $("#ploog").remove();
+    ploog.started = false;
 };
 
 (function($) {
-	if (typeof $.fn.drags == "undefined"){
-    $.fn.drags = function(opt) {
+    if (typeof $.fn.drags == "undefined") {
+        $.fn.drags = function(opt) {
 
-        opt = $.extend({handle:"",cursor:"move"}, opt);
+            opt = $.extend({
+                handle: "",
+                cursor: "move"
+            }, opt);
 
-        if(opt.handle === "") {
-            var $el = this;
-        } else {
-            var $el = this.find(opt.handle);
-        }
-
-        return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
-            if(opt.handle === "") {
-                var $drag = $(this).addClass('draggable');
+            if (opt.handle === "") {
+                var $el = this;
             } else {
-                var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+                var $el = this.find(opt.handle);
             }
-            var z_idx = $drag.css('z-index'),
-                drg_h = $drag.outerHeight(),
-                drg_w = $drag.outerWidth(),
-                pos_y = $drag.offset().top + drg_h - e.pageY,
-                pos_x = $drag.offset().left + drg_w - e.pageX;
-            $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
-                $('.draggable').offset({
-                    top:e.pageY + pos_y - drg_h,
-                    left:e.pageX + pos_x - drg_w
-                }).on("mouseup", function() {
-                    $(this).removeClass('draggable').css('z-index', z_idx);
+
+            return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+                if (opt.handle === "") {
+                    var $drag = $(this).addClass('draggable');
+                } else {
+                    var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+                }
+                var z_idx = $drag.css('z-index'),
+                    drg_h = $drag.outerHeight(),
+                    drg_w = $drag.outerWidth(),
+                    pos_y = $drag.offset().top + drg_h - e.pageY,
+                    pos_x = $drag.offset().left + drg_w - e.pageX;
+                $drag.css('z-index', 1000).parents().on("mousemove", function(e) {
+                    $('.draggable').offset({
+                        top: e.pageY + pos_y - drg_h,
+                        left: e.pageX + pos_x - drg_w
+                    }).on("mouseup", function() {
+                        $(this).removeClass('draggable').css('z-index', z_idx);
+                    });
                 });
+                e.preventDefault(); // disable selection
+            }).on("mouseup", function() {
+                if (opt.handle === "") {
+                    $(this).removeClass('draggable');
+                } else {
+                    $(this).removeClass('active-handle').parent().removeClass('draggable');
+                }
             });
-            e.preventDefault(); // disable selection
-        }).on("mouseup", function() {
-            if(opt.handle === "") {
-                $(this).removeClass('draggable');
-            } else {
-                $(this).removeClass('active-handle').parent().removeClass('draggable');
-            }
-        });
 
-    }}else {console.log("drags already exists");}
+        }
+    } else {
+        console.log("drags already exists");
+    }
 })(jQuery);
 
-if (!ploog.started){
-	ploog.init();
+if (!ploog.started) {
+    ploog.init();
 }
