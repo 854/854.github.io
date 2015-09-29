337af921d5936efc961d41394d83dbfc2b31826d
Matt Thompson <matthewaaronthompson@gmail.com>
2015-09-29 09:35:23 -0500

    no inline images

--- autodub.js
+++ autodub.js
@@ -1,6 +1,7 @@
 var autoDub = {
 	started: false,
-	version: "00.02"
+	version: "00.03",
+	blockInlineImages: true
 }
 
 autoDub.newSong = function(){
@@ -10,8 +11,20 @@ autoDub.newSong = function(){
 	console.log("voted for "+songName);
 };
 
+autoDub.newChat = function(data){
+	try{
+		var url = data.message;
+ 		if ((url.slice(-4) == '.png') || (url.slice(-4) == '.jpg') || (url.slice(-4) == '.gif')) {
+ 			$(".chat-main").find("p:last").html("<a href=\""+data.message+"\">"+data.message+"</a>");
+ 		}
+	} catch (e){
+
+	}
+};
+
 autoDub.init = function(){
 	$('.currentSong').bind("DOMSubtreeModified", autoDub.newSong);
+	Dubtrack.Events.bind("realtime:chat-message", autoDub.newChat);
 	$(".dubup").click();
 	console.log("autodub v"+autoDub.version+" is a go!");
 };
