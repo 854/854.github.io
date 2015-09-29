3aa4ced5eb5f0c4bf2156b587ce62733521a4b3c
Matt Thompson <matthewaaronthompson@gmail.com>
2015-09-29 10:45:56 -0500

    settings

--- autodub.js
+++ autodub.js
@@ -1,7 +1,8 @@
 var autoDub = {
 	started: false,
-	version: "00.03",
-	blockInlineImages: true
+	version: "00.04",
+	blockInlineImages: true,
+	autoVote: true
 }
 
 autoDub.newSong = function(){
@@ -14,18 +15,52 @@ autoDub.newSong = function(){
 autoDub.newChat = function(data){
 	try{
 		var url = data.message;
- 		if ((url.slice(-4) == '.png') || (url.slice(-4) == '.jpg') || (url.slice(-4) == '.gif')) {
- 			if (autoDub.blockInlineImages) $(".chat-main").find("p:last").html("<a href=\""+data.message+"\">"+data.message+"</a>");
+ 		if ((url.slice(-4) == '.png') || (url.slice(-4) == '.jpg') || (url.slice(-4) == '.gif') || (url.slice(-5) == '.jpeg')) {
+ 			if (autoDub.blockInlineImages) $(".chat-main").find("p:last").html("<a class=\"autolink\" target=\"_blank\" href=\""+data.message+"\">"+data.message+"</a>");
  		}
 	} catch (e){
 
 	}
 };
 
+autoDub.storage = {
+	save: function(){
+		var save_file = {
+			blockInlineImages: autoDub.blockInlineImages,
+			autoVote: autoDub.autoVote
+		};
+		var preferences = JSON.stringify(save_file);
+		localStorage["autoDub"] = preferences;
+	},
+	restore: function(){
+		var favorite = localStorage["autoDub"];
+ 		if (!favorite) {
+    		return;
+ 		}
+ 		var preferences = JSON.parse(favorite);
+ 		$.extend(autoDub, preferences);
+	}
+};
+
+autoDub.toggleInline = function(){
+	if (autoDub.blockInlineImages){
+		autoDub.blockInlineImages = false;
+		$(".autodub-link").text("[ADub] Chat Images: OFF");
+	} else {
+		autoDub.blockInlineImages = true;
+		$(".autodub-link").text("[ADub] Chat Images: ON");
+	}
+	autoDub.storage.save();
+};
+
 autoDub.init = function(){
+	autoDub.storage.restore();
 	$('.currentSong').bind("DOMSubtreeModified", autoDub.newSong);
 	Dubtrack.Events.bind("realtime:chat-message", autoDub.newChat);
 	$(".dubup").click();
+	var onOff = "OFF";
+	if (autoDub.blockInlineImages) onOff = "ON";
+	$("#main-menu-left").append("<a href=\"#\" onclick=\"autoDub.toggleInline()\" class=\"autodub-link\">[ADub] Chat Images: "+onOff+"</a>");
 	console.log("autodub v"+autoDub.version+" is a go!");
 };
 
