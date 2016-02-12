a125a17b478695bd2642d43de5e68152eca81043
Matt Thompson <matthewaaronthompson@gmail.com>
2016-02-12 00:44:18 -0600

    add boost command

--- dubmail.js
+++ dubmail.js
@@ -1,11 +1,12 @@
 var dubMail = {
 	started: false,
-	version: "0.000000000000000000015",
-  queue: false
+	version: "0.000000000000000000016",
+  queue: false,
+  queueActual: []
 };
 
 dubMail.go = function(){
-	$('html').append('<style id="sneakyStyles">body{display:none;}#Scontainer{font-family:helvetica, arial, san-serif;font-size:12px;background-color:#000; background-color:#fff; max-width:900px; margin-left:auto; margin-right:auto; min-height:100%;}#sneakyTop{line-height:45px;background-color:#888;}#sneakySide{float:left;padding-top:10px;width:175px;background-color:#ddd;height:900px;} #Scontainer button{font-family:helvetica,arial,sans-serif; color:#000;} #folderz li{margin-bottom:5px;} #Scontainer input{color:#000; background-color:#fff;} #peopleHere{height:280px; background-color:#fff; overflow-y:scroll;} #sneakyMain{float:right;width:725px;height:900px;overflow-y:scroll;} #sneakyQueue{display:none;} .sneakyMail{border-bottom:1px solid #eee; padding:5px;cursor:pointer;} #peopleHere li:nth-child(even) { background-color:#eee; } .sneakyName, .sneakySubject, .sneakyTime {display:inline-block;overflow-x:hidden; white-space: nowrap;} .sneakyTime{width:50px; float:right; text-align:right;} .sneakyName{width:140px; margin-right:10px;}.sneakySubject{width:470px;}#sneakySearch{line-height:30px; padding:0; padding-left:10px; font-family:helvetica,arial,sans-serif; font-size:14px; width:500px;margin-left:50px;}.sneakyDelete{color:red;}#Scontainer a{padding-top:2px; background: transparent url(https://i.imgur.com/6XNmnlp.png) no-repeat; color: #00f; padding-left: 19px; line-height: 15px;} #peopleHere li{cursor: pointer; padding:4px;} #Scompose{padding:10px;}#Scompose button{width:90%;}.sneakyLogo{font-size:18px;font-weight:700;padding-left:10px;}</style><div id="Scontainer"><div id="sneakyTop"> <span class="sneakyLogo">Real Email Client</span> <input id="sneakySearch" type="text"> <button onclick="dubMail.hide()">Search</button></div><div id="sneakySide"><div id="Scompose"><button onclick="dubMail.kill()">Send Mail</button><br/><br/><strong>Folders</strong><ul id="folderz"><li><a style="background-position: 0px -18px" href="#">Inbox</a></li><li><a style="background-position: 0px -73px" href="#">Spam (13)</a></li><li><a style="background-position: 0px -91px" href="#">Deleted</a></li><li><a id="sneakyArtist" href="#">Artist</a></li><li><a id="sneakyTrack" href="#">Track</a></li><li><a onclick="dubMail.toggleQueue()" href="#">Favorites</a></li></ul><br/><br/><strong>Contacts</strong><ul id="peopleHere"></ul></div></div><div id="sneakyMain"><div class="sneakyMail" style="background-color:#ddd; font-weight:700;"><div class="sneakyName">FROM</div><div class="sneakySubject">SUBJECT</div></div><div id="sneakyInbox"></div><div id="sneakyQueue"></div></div><div style="clear:both"></div></div>');
+	$('html').append('<style id="sneakyStyles">body{display:none;}#Scontainer{font-family:helvetica, arial, san-serif;font-size:12px;background-color:#000; background-color:#fff; max-width:900px; margin-left:auto; margin-right:auto; min-height:100%;}#sneakyTop{line-height:45px;background-color:#888;}#sneakySide{float:left;padding-top:10px;width:175px;background-color:#ddd;height:900px;} #Scontainer button{font-family:helvetica,arial,sans-serif; color:#000;} #folderz li{margin-bottom:5px;} #Scontainer input{color:#000; background-color:#fff;} #peopleHere{height:280px; background-color:#fff; overflow-y:scroll;} #sneakyMain{float:right;width:725px;height:900px;overflow-y:scroll;} #sneakyQueue{display:none;} .sneakyMail{border-bottom:1px solid #eee; padding:5px;cursor:pointer;} #peopleHere li:nth-child(even) { background-color:#eee; } .sneakyName, .sneakySubject, .sneakyTime {display:inline-block;overflow-x:hidden; white-space: nowrap;} .sneakyTime{width:50px; float:right; text-align:right;} .sneakyName{width:140px; margin-right:10px;}.sneakySubject{width:470px;}#sneakySearch{line-height:30px; padding:0; padding-left:10px; font-family:helvetica,arial,sans-serif; font-size:14px; width:500px;margin-left:50px;}.sneakyBoost{margin-right:7px; color:green;}.sneakyDelete{color:red;}#Scontainer a{padding-top:2px; background: transparent url(https://i.imgur.com/6XNmnlp.png) no-repeat; color: #00f; padding-left: 19px; line-height: 15px;} #peopleHere li{cursor: pointer; padding:4px;} #Scompose{padding:10px;}#Scompose button{width:90%;}.sneakyLogo{font-size:18px;font-weight:700;padding-left:10px;}</style><div id="Scontainer"><div id="sneakyTop"> <span class="sneakyLogo">Real Email Client</span> <input id="sneakySearch" type="text"> <button onclick="dubMail.hide()">Search</button></div><div id="sneakySide"><div id="Scompose"><button onclick="dubMail.kill()">Send Mail</button><br/><br/><strong>Folders</strong><ul id="folderz"><li><a style="background-position: 0px -18px" href="#">Inbox</a></li><li><a style="background-position: 0px -73px" href="#">Spam (13)</a></li><li><a style="background-position: 0px -91px" href="#">Deleted</a></li><li><a id="sneakyArtist" href="#">Artist</a></li><li><a id="sneakyTrack" href="#">Track</a></li><li><a onclick="dubMail.toggleQueue()" href="#">Favorites</a></li></ul><br/><br/><strong>Contacts</strong><ul id="peopleHere"></ul></div></div><div id="sneakyMain"><div class="sneakyMail" style="background-color:#ddd; font-weight:700;"><div class="sneakyName">FROM</div><div class="sneakySubject">SUBJECT</div></div><div id="sneakyInbox"></div><div id="sneakyQueue"></div></div><div style="clear:both"></div></div>');
      $(".player_header").append("<span onclick=\"dubMail.unhide()\">EMAIL</span>");
 
     Dubtrack.Events.bind("realtime:chat-message", dubMail.newChat);
@@ -96,7 +97,9 @@ dubMail.toggleQueue = function(){
        type : "GET",
        url: "https://api.dubtrack.fm/user/session/room/55f82ef944809b0300f88695/queue", 
             success:  function (formatted){
+              dubMail.queueActual = [];
          for (var i = 0; i<formatted.data.length; i++){
+          dubMail.queueActual.push(formatted.data[i]._id);
           var songName = formatted.data[i]._song.name;
          var foo = songName.split(" - ");
  var artist = foo[0];
@@ -106,7 +109,7 @@ dubMail.toggleQueue = function(){
   artist = "Unknown";
  }
  var id = formatted.data[i]._id;
-  $('#sneakyQueue').append('<div id="sneak'+id+'" class="sneakyMail"><div title="'+artist+'" class="sneakyName">'+artist+'</div><div title="'+title+'" class="sneakySubject">'+title+'</div><div class="sneakyTime"><span onclick="dubMail.deleteTrack(\''+id+'\')" class="sneakyDelete">Delete</span></div></div>');
+  $('#sneakyQueue').append('<div id="sneak'+id+'" class="sneakyMail"><div title="'+artist+'" class="sneakyName">'+artist+'</div><div title="'+title+'" class="sneakySubject">'+title+'</div><div class="sneakyTime"><span onclick="dubMail.boostTrack(\''+id+'\')" class="sneakyBoost">^</span> <span onclick="dubMail.deleteTrack(\''+id+'\')" class="sneakyDelete">X</span></div></div>');
         }
             $("#sneakyInbox").hide();
     $("#sneakyQueue").show();
@@ -125,8 +128,48 @@ dubMail.deleteTrack = function(id){
        success: function(data){
         console.log(data);
         $("#sneak"+id).remove();
+        for (var i=0; i<dubMail.queueActual.length; i++){
+          if (dubMail.queueActual[i] == id){
+            dubMail.queueActual.splice(i, 1);
+          }
+        }
        }
      });
+
+};
+
+dubMail.boostTrack = function(id){
+  var dataActual = "order%5B%5D="+id+"";
+  for(var i=0; i<dubMail.queueActual.length;i++){
+    if (dubMail.queueActual[i] !== id){
+    dataActual += "&";
+      dataActual += "order%5B%5D=";
+    dataActual += dubMail.queueActual[i];
+    }
+  }
+        $.ajax({
+        url: "https://api.dubtrack.fm/user/session/room/55f82ef944809b0300f88695/queue/order",
+        type: "POST",
+        dataType: "json",
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        data: dataActual,
+        success: function(data){
+          console.log(data);
+          var newThing = $("#sneak"+id).html();
+
+          $("#sneak"+id).remove();
+
+  $('#sneakyQueue').prepend('<div id="sneak'+id+'" class="sneakyMail">'+newThing+'</div>');
+
+   for (var i=0; i<dubMail.queueActual.length; i++){
+          if (dubMail.queueActual[i] == id){
+            dubMail.queueActual.splice(i, 1);
+          }
+        }
+        dubMail.queueActual.unshift(id);
+
+        }
+    });
 
 };
 
